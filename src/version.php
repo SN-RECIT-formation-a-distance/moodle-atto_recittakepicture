24b99c2c38184494ccbc9bef16dfbaebd8eb2197
Gustavo Bazzo <gcbazzo@gmail.com>
2021-05-04 10:29:41 -0400

    Update version.php

--- src/version.php
+++ src/version.php
@@ -20,5 +20,5 @@ $plugin->version   = 2021030200;
 $plugin->requires  = 2018050800;
 $plugin->component = 'atto_recittakepicture';
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'R12-V1.12.3';
+$plugin->release = 'R13-V1.13.0';
 
